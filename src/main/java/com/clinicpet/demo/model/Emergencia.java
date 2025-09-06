045996b9ce8f535deae030880b9f41a6416abce2
Diana Valentina Pacacira Montoya <diana.valentian6002@gmail.com>
2025-09-06 11:46:23 -0500

    EmergenciaEventoHistoriaClinicaInventarioNotificacionPagoPaqueteModel_valentina_060902025

--- src/main/java/com/clinicpet/demo/model/Emergencia.java
+++ src/main/java/com/clinicpet/demo/model/Emergencia.java
@@ -1,5 +1,105 @@
 package com.clinicpet.demo.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Emergencia")
 public class Emergencia {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String tipo;
+	private LocalDateTime fechayhora;
+	private String descripcion;
+
+	@ManyToOne
+	@JoinColumn(name = "idMascota", nullable = false)
+	private Mascota mascota;
+
+	@ManyToOne
+	@JoinColumn(name = "idVeterinario", nullable = false)
+	private Usuario veterinario;
+
+	// constructor sin campos
+	public Emergencia() {
+
+	}
+
+	// constructor con campos
+	public Emergencia(Integer id, String tipo, LocalDateTime fechayhora, String descripcion, Mascota mascota,
+			Usuario veterinario) {
+		super();
+		this.id = id;
+		this.tipo = tipo;
+		this.fechayhora = fechayhora;
+		this.descripcion = descripcion;
+		this.mascota = mascota;
+		this.veterinario = veterinario;
+	}
+
+	// getters & setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public LocalDateTime getFechayhora() {
+		return fechayhora;
+	}
+
+	public void setFechayhora(LocalDateTime fechayhora) {
+		this.fechayhora = fechayhora;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Mascota getMascota() {
+		return mascota;
+	}
+
+	public void setMascota(Mascota mascota) {
+		this.mascota = mascota;
+	}
+
+	public Usuario getVeterinario() {
+		return veterinario;
+	}
+
+	public void setVeterinario(Usuario veterinario) {
+		this.veterinario = veterinario;
+	}
+
+	// to string
+	@Override
+	public String toString() {
+		return "Emergencia [id=" + id + ", tipo=" + tipo + ", fechayhora=" + fechayhora + ", descripcion=" + descripcion
+				+ ", mascota=" + mascota + ", veterinario=" + veterinario + "]";
+	}
 
 }
--- src/main/java/com/clinicpet/demo/model/Evento.java
+++ src/main/java/com/clinicpet/demo/model/Evento.java
@@ -1,5 +1,86 @@
 package com.clinicpet.demo.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Evento")
 public class Evento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String titulo;
+	private String descripcion;
+	private LocalDateTime fecha;
+	private String imagen;
+
+	// constructor vacio
+	public Evento() {
+
+	}
+
+	// constructor con campos
+	public Evento(Integer id, String titulo, String descripcion, LocalDateTime fecha, String imagen) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descripcion = descripcion;
+		this.fecha = fecha;
+		this.imagen = imagen;
+	}
+
+	// getters & setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	// to string
+	@Override
+	public String toString() {
+		return "Evento [id=" + id + ", titulo=" + titulo + ", descripcion=" + descripcion + ", fecha=" + fecha
+				+ ", imagen=" + imagen + "]";
+	}
 
 }
