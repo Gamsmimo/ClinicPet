16da53c82863a5054b3a1dddb13848ab15db3e03
valentina <diana.valentina6002@gmail.com>
2025-09-15 16:51:34 -0500

    Actualizacion_Datos_Valentina

--- src/main/java/com/clinicpet/demo/repository/IVeterinariaRepository.java
+++ src/main/java/com/clinicpet/demo/repository/IVeterinariaRepository.java
@@ -10,8 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public interface IVeterinariaRepository extends JpaRepository<Veterinaria, Integer> {
