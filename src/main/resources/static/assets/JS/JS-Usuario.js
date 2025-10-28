ba60e5e43995af4d43a7794e9cde194d93ef9f6d
mimmo31 <crissbecerra31032003@gmail.com>
2025-10-28 14:20:07 -0500

    CorrecionesDeErroresEnRespositorioCristian_mimmo31

--- src/main/resources/static/assets/JS/JS-Usuario.js
+++ src/main/resources/static/assets/JS/JS-Usuario.js
@@ -1063,7 +1063,6 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 //Script para eliminar mascota
-<<<<<<< HEAD
 
 // Variable global para almacenar el archivo temporal
 let currentPhotoFile = null;
@@ -1177,8 +1176,6 @@ async function saveProfilePicture() {
 	}
 }
 
-=======
->>>>>>> 37753e4888d2b994ec002e62ae88d141d42d0788
 function eliminarMascota(id) {
 	if (confirm('¿Estás seguro de que quieres eliminar esta mascota?')) {
 		fetch('/usuarios/perfilusuario/eliminarmascota/' + id, {
@@ -1206,7 +1203,6 @@ function eliminarMascota(id) {
 }
 
 
-<<<<<<< HEAD
 // Funciones para manejar la foto de perfil
 function previewProfilePicture(input) {
 	if (input.files && input.files[0]) {
@@ -1429,6 +1425,5 @@ function showAlert(message, type) {
 		}
 	}, 5000);
 }
-=======
->>>>>>> 37753e4888d2b994ec002e62ae88d141d42d0788
+
 
